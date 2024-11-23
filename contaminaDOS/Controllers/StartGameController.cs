00054346d90fbc79d5d8ace8368fbf77edcfac05
Brandon <brandonvargass12345@gmail.com>
2024-11-23 17:35:00 -0600

    x-msg minuscula

--- contaminaDOS/Controllers/StartGameController.cs
+++ contaminaDOS/Controllers/StartGameController.cs
@@ -62,7 +62,7 @@ namespace contaminaDOS.Controllers
 
         private IActionResult CreateErrorResponse(string message, int statusCode)
         {
-            Response.Headers.Add("X-msg", message);
+            Response.Headers.Add("x-msg", message);
             return StatusCode(statusCode);
         }
     }
